8beefe9217660be4a149e9f85a61e1e379824314
JhonH24 <jhonh0807@gmail.com>
2022-09-23 13:37:22 -0300

    Class Envase

---
+++ asset/Envase.js
@@ -0,0 +1,60 @@
+const Envase = class Envase {
+  constructor(tipoEnvase, capacidad, diametro, valor, material) {
+    this.tipoEnvase = tipoEnvase;
+    this.capacidad = capacidad;
+    this.diametro = diametro;
+    this.valor = valor;
+    this.material = material;
+  }
+
+  get TipoEnvase() {
+    return this.tipoEnvase;
+  }
+
+  set TipoEnvase(tipoEnvase) {
+    this.tipoEnvase = tipoEnvase;
+  }
+
+  get Capacidad() {
+    return this.capacidad;
+  }
+
+  set Capacidad(capacidad) {
+    this.capacidad = capacidad;
+  }
+
+  get Diametro() {
+    return this.diametro;
+  }
+
+  set Diametro(diametro) {
+    this.diametro = diametro;
+  }
+
+  get Valor() {
+    return this.valor;
+  }
+
+  set Valor(valor) {
+    this.valor = valor;
+  }
+
+  get Material() {
+    return this.material;
+  }
+
+  set Material(material) {
+    this.material = material;
+  }
+
+  mostrarDatos() {
+    console.log("Datos del envase: ");
+    console.log("Tipo: " + this.getTipoEnvase);
+    console.log("Tipo: " + this.getCapacidad());
+    console.log("Tipo: " + this.getDiametrio());
+    console.log("Tipo: " + this.getMaterial());
+    console.log("Valor Devolucion X Un: $ " + this.valor.toFixed(2));
+  }
+};
+
+module.exports = Envase;
---
+++ asset/Panel.js
@@ -0,0 +1,6 @@
+//Bloque Declarativo
+
+let envase01 = new Envase("Botella", "1L", "5x15cm", 50, "Vidrio");
+let envase02 = new Envase("Lata", "500ml", "3x7cm", 25, "Aluminio");
+let envase03 = new Envase("Botella", "3L", "10x40cm", 100,"Plastico");
+let envase04 = new Envase("Botella", "1.5L", "10x20cm", 75, "Plastico");
